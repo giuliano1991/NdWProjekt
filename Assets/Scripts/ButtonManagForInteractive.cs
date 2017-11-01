ca1a697ce8ec2f9bb0b2731c07d5230ede423606
Nana <tietje.hh@gmail.com>
2017-11-02 00:59:13 +0100

    interactives und statisches Menü hinzugefügt, Prefab Ordner für int.Menü hinzugefügt

---
+++ Assets/Scripts/ButtonManagForInteractive.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManagForInteractive : MonoBehaviour {
+
+	public string unloadOutro;
+
+	public void LoadMainMenu() {
+
+		// hier Main Menu Szene laden
+
+	}
+
+	void Start () {
+	
+		if (unloadOutro != "") {
+
+			StartCoroutine (UnloadScene (unloadOutro));
+
+		}
+
+	}
+
+	IEnumerator UnloadScene(string unload) {
+		yield return new WaitForSeconds (0.1f);
+		SceneManag.Instance.Unload (unload);
+	}
+
+}
---
+++ Assets/Scripts/MenuToggle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using VRTK;
+
+public class MenuToggle : MonoBehaviour {
+    public VRTK_ControllerEvents controllerEvents;
+    public GameObject menu;
+
+    private bool isShown = false;
+
+    private void OnEnable()
+    {
+        controllerEvents.ButtonTwoPressed += ControllerEvents_ButtonTwoPressed;
+        controllerEvents.ButtonTwoReleased += ControllerEvents_ButtonTwoReleased;
+    }
+
+    private void OnDisable()
+    {
+        controllerEvents.ButtonTwoPressed -= ControllerEvents_ButtonTwoPressed;
+        controllerEvents.ButtonTwoReleased -= ControllerEvents_ButtonTwoPressed;
+    }
+
+    private void ControllerEvents_ButtonTwoReleased(object sender, ControllerInteractionEventArgs e)
+    {
+        isShown = !isShown;
+        menu.SetActive(isShown);
+    }
+
+    private void ControllerEvents_ButtonTwoPressed(object sender, ControllerInteractionEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
