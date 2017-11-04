121a0860d4b94e7ef1a705297f05bf3ee1d027b4
Nana <tietje.hh@gmail.com>
2017-11-04 17:51:17 +0100

    fixed

--- Assets/VRTK/Examples/ExampleResources/Scripts/VRTK_ControllerEvents_ListenerExample.cs
+++ Assets/VRTK/Examples/ExampleResources/Scripts/VRTK_ControllerEvents_ListenerExample.cs
@@ -4,10 +4,8 @@
 
     public class VRTK_ControllerEvents_ListenerExample : MonoBehaviour
     {
-
         private void Start()
         {
-
             if (GetComponent<VRTK_ControllerEvents>() == null)
             {
                 VRTK_Logger.Error(VRTK_Logger.GetCommonMessage(VRTK_Logger.CommonMessageKeys.REQUIRED_COMPONENT_MISSING_FROM_GAMEOBJECT, "VRTK_ControllerEvents_ListenerExample", "VRTK_ControllerEvents", "the same"));
